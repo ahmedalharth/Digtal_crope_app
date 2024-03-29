746faeb55e7fa513a8dea9ee7561358a0734c2cf
ahmedalharth <osamaahmedalharth@gmail.com>
2024-03-30 01:33:54 +0200

    modified

--- app.py
+++ app.py
@@ -141,14 +141,12 @@ def find_highly_correlated_features(data, threshold=0.6):
 # Custom theme configuration
 
 # Set the Streamlit theme
-st.write("""
-[theme]
+st.write("""[theme]
 base="dark"
 backgroundColor="#155049"
 secondaryBackgroundColor="#0e290e"
 font="monospace"
 """, unsafe_allow_html=True)
-
 
 # Title
 st.title("Digtal Crope Estimation Problem ")
